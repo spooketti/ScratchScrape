669365c5e6082f7b13900e88d4a66e8717ff9083
spooketti <jonliu168@gmail.com>
2025-01-23 09:00:55 -0800

    gets all the stuff and iterates per sprite

--- codeanalysis.py
+++ codeanalysis.py
@@ -3,12 +3,21 @@ import json
 with open("projects/1072618382.json","r") as file:
     data = json.load(file)
     
-somedataidk = len(data['targets'])
+numOfSprites = len(data['targets']) #includes the stage as well
 blocks_with_no_parent = list()
 
-for i in range(somedataidk):
-    blocks = data['targets'][i]['blocks']
-    blocks_with_no_parent += list(filter(lambda item: item[1].get("parent") is None, blocks.items()))
 
-for blockid, blockData in blocks_with_no_parent:
-    print(blockData)
+for i in range(numOfSprites):
+    
+    def followCodeChain(blockData):
+        print(blockData["opcode"])
+        if(blockData["next"] == None):
+            return
+        followCodeChain(data["targets"][i]["blocks"][blockData["next"]])
+
+
+    blocks = data['targets'][i]['blocks']
+    blocks_with_no_parent = list(filter(lambda item: item[1].get("parent") is None, blocks.items()))
+    for blockid, blockData in blocks_with_no_parent:
+        followCodeChain(blockData)
+
