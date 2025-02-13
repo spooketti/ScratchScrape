df220b9083e0bb68871ca2564b34a52741ef3b19
mura2432 <houcheng.jg@gmail.com>
2025-02-12 23:01:51 -0800

    its oogaboogaing

--- oogaboogathingydoer.py
+++ oogaboogathingydoer.py
@@ -1,20 +1,32 @@
 import bs4
+import json
+import matplotlib.pyplot as plt
+import os
 import requests
-import time
 import re
 import random
-from main import grabProjectData
+import time
 import tkinter as tk
 
-# sets the start and end link indexes
-def search(start, end):
+def search():
+    # sets the start and end link indexes
+    start = (int)(entry1.get())
+    end = (int)(entry2.get())
+
+    # print("bounds acquired", start, end)
+
     for i in range(start, end):
+        status_label.config(text="Currently On Class:" + str(i))
+        root.update()
+        
         # get page with studios for each class
         url = 'https://scratch.mit.edu/classes/'+ str(i) +'/studios/'
         page = requests.get(url)
         
         # sleep delay to avoid getting flagged
         time.sleep(random.uniform(0.4, 0.6))
+
+        # print("page acquired")
 
         # grab working studio numbers
         soup = bs4.BeautifulSoup(page.text, 'html.parser')
@@ -26,6 +38,8 @@ def search(start, end):
             if(number != 0):
                 studio_set.add(number)
         
+        # print("checking studio_set")
+ 
         # check each studio
         for studio in studio_set:
             # use href to get studio number out of the html
@@ -39,28 +53,106 @@ def search(start, end):
             # sleep delay to avoid getting flagged
             time.sleep(random.uniform(0.4, 0.6))
 
+            # print("being json data parse")
+
             # download as json to more easily parse ids
             json_data = page2.json()
             for data in json_data:
-                with open("output.txt", "a") as file:
-                    file.write(str(data['id']) + "\n")
+                # with open("output.txt", "a") as file:
+                #     file.write(str(data['id']) + "\n")
 
-# Create main window
+                curr_project = requests.get("https://api.scratch.mit.edu/projects/" + str(data['id'])).json()
+                processProjectFile(curr_project['id'], curr_project['project_token'])
+            
+            # print("json data parse end")
+    
+    root.destroy()
+
+logicComplexity = {"repeat":0, #
+                   "variables":0,
+                   "functions":0,
+                   "conditionals":0,
+                   "arithmetic":0,
+                   "comparison":0, # < > =
+                   "complexMath":0, #anything in the menu that lets you do cos sin atan etc
+                   "scriptCount":0} #defined by how many blocks have no parents (start of a script)
+
+def processProjectFile(projectId, projectToken):
+    file = requests.get("https://projects.scratch.mit.edu/"+str(projectId)+"?token="+str(projectToken))
+    data = file.json()
+
+    try:
+        numOfSprites = len(data['targets'])
+    except:
+        print("too old")
+    else:
+        blocks_with_no_parent = list()
+
+        for i in range(numOfSprites):
+            def followCodeChain(blockData):
+                if(blockData["opcode"] in ["control_repeat","control_repeat_until"] ):
+                    logicComplexity["repeat"] += 1
+                #i dont know how to find functions so u can have fun james
+                
+                if(blockData["opcode"] in ["control_if", "control_if_else"]):
+                    logicComplexity["conditionals"] += 1
+                    
+                if(blockData["opcode"] in ["operator_add", "operator_subtract", "operator_multiply", "operator_divide"]):
+                    logicComplexity["arithmetic"] += 1
+                
+                if(blockData["opcode"] in ["operator_equals", "operator_lt", "operator_gt"]):
+                    logicComplexity["comparison"] += 1
+                    
+                if(blockData["opcode"] in ["operator_mathop"]):
+                    logicComplexity['complexMath'] += 1
+                
+                # followCodeChain(data["targets"][i]["blocks"][blockData["next"]])
+                
+            logicComplexity["variables"] += len(data["targets"][i]["variables"])
+
+            blocks = data['targets'][i]['blocks']
+            # print(data["targets"][i]["name"])
+            
+            blocks_with_no_parent = list(filter(lambda item: item[1].get("parent") is None, blocks.items()))
+            logicComplexity["scriptCount"] += len(blocks_with_no_parent)
+            
+            for j in blocks:
+                followCodeChain(blocks[j])
+
+def graphAndSave():
+    print(logicComplexity)
+
+    
+
+    plt.bar(range(len(logicComplexity)), list(logicComplexity.values()), align='center')
+    plt.xticks(range(len(logicComplexity)), list(logicComplexity.keys()))
+    plt.title("Scratch User Project Complexity")
+    plt.show()
+
+def gui():
+    # Create main window
+    root.title("Web Scrape")
+
+    # Create input fields and labels
+    tk.Label(root, text="Enter start index(Note that 5 years ago was 255000):").grid(row=0, column=0)
+    tk.Label(root, text="Enter end index:").grid(row=1, column=0)
+
+    entry1.grid(row=0, column=1)
+    entry2.grid(row=1, column=1)
+
+    status_label.grid(row=3, column=0, columnspan=2)
+
+    # Create button to calculate
+    button = tk.Button(root, text="Start", command=search)
+    button.grid(row=2, column=0, columnspan=2)
+
+    # Run the GUI
+    root.mainloop()
+
 root = tk.Tk()
-root.title("Web Scrape")
-
-# Create input fields and labels
-tk.Label(root, text="Enter start index:").grid(row=0, column=0)
-tk.Label(root, text="Enter end index:").grid(row=1, column=0)
-
 entry1 = tk.Entry(root)
 entry2 = tk.Entry(root)
-entry1.grid(row=0, column=1)
-entry2.grid(row=1, column=1)
+status_label = tk.Label(root, text="Waiting for Input")
+gui()
 
-# Create button to calculate
-button = tk.Button(root, text="Start", command=search)
-button.grid(row=2, column=0, columnspan=2)
-
-# Run the GUI
-root.mainloop()
+graph()
